79413c75d72c38346fc9442ad2214332d370d36d
Ankit Gautam <antam122508@gmail.com>
2025-03-21 11:18:53 +0530

    Pin_Code_1

--- pin_Code.js
+++ pin_Code.js
@@ -1,13 +1,10 @@
 // Function to validate Indian PIN code
 const validatePinCode = (pinCode) => {
-    return /^\d{3}\s?\d{3}$/.test(pinCode); // Allows both '400088' and '400 088'
+    return /^\d{6}$/.test(pinCode);
 };
 
 // Example usage of PIN code validation
-console.log(validatePinCode("400088"));  // true
-console.log(validatePinCode("400 088")); // true
-console.log(validatePinCode("4008"));    // false
+console.log(validatePinCode("400088")); // true
+console.log(validatePinCode("4008"));   // false
 console.log(validatePinCode("4000888")); // false
 console.log(validatePinCode("40A088"));  // false
-console.log(validatePinCode("A400088")); // false
-console.log(validatePinCode("400088B")); // false
